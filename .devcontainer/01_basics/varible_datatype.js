39642f40eddb9a03fe350dacabbb196272571c69
SanjayRanka2002 <samratking120@gmail.com>
2024-09-15 08:17:37 +0530

    add a code

---
+++ .devcontainer/01_basics/varible_datatype.js
@@ -0,0 +1,11 @@
+const regid = 34
+let username = "sanjay"
+var useremail = "sanjayranka"
+usercity = "jodhpur"
+
+username = "Shree Ram"
+useremail = "shreeram422"
+usercity = "jodhana"
+
+console.table([regid,username,useremail,usercity])
+/* prefer not to use var for issue in block scope or function scope */
